8fe629a071ef9c4784f851e1acdcd859674fdf7a
Katya <katyakhomenko2105@gmail.com>
2024-09-07 22:54:44 +0300

    one commit

---
+++ src/js/faq/faq.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.toggle-btn').forEach(button => {
+  button.addEventListener('click', () => {
+    const faqContent = button.nextElementSibling;
+    const isOpen = faqContent.style.display === 'block';
+
+    
+    faqContent.style.display = isOpen ? 'none' : 'block';
+    
+    button.textContent = isOpen ? '▼' : '▲';
+  });
+});
