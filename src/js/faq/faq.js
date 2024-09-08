d5cdf6a3bb8bf8fa9093476abeb94d010d79f7ec
Katya <katyakhomenko2105@gmail.com>
2024-09-08 18:20:44 +0300

    commit

--- src/js/faq/faq.js
+++ src/js/faq/faq.js
@@ -1,14 +1,4 @@
-// document.querySelectorAll('.toggle-btn').forEach(button => {
-//   button.addEventListener('click', () => {
-//     const faqContent = button.nextElementSibling;
-//     const isOpen = faqContent.style.display === 'block';
-
-//     faqContent.style.display = isOpen ? 'none' : 'block';
-
-//     // button.textContent = isOpen ? '▼' : '▲';
-//   });
-// });
-
+//
 document.querySelectorAll('.toggle-btn').forEach(button => {
   button.addEventListener('click', () => {
     const faqContent = button.nextElementSibling;
@@ -16,7 +6,6 @@ document.querySelectorAll('.toggle-btn').forEach(button => {
 
     faqContent.style.display = isOpen ? 'none' : 'block';
 
-    // Переключение класса для вращения
     button.classList.toggle('rotated');
   });
 });
