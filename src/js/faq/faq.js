3fab442dfb41714ad8f78a14c11da64c118d88b0
Olga Oliynik <poddubnaolga@gmail.com>
2024-09-09 20:02:57 +0300

    Fix 2

--- src/js/faq/faq.js
+++ src/js/faq/faq.js
@@ -2,7 +2,7 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 const accordion = new Accordion('.faq-list', {
-  duration: 250,
+  duration: 500,
   showMultiple: true,
   triggerClass: "faq-item-button",
 });
