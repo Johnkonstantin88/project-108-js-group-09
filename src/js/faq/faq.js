d4164e044d2e1aebd65619c045931c295e82eb25
Olga Oliynik <poddubnaolga@gmail.com>
2024-09-09 10:16:57 +0300

    All from begining

--- src/js/faq/faq.js
+++ src/js/faq/faq.js
@@ -1,11 +1,23 @@
-//
-document.querySelectorAll('.toggle-btn').forEach(button => {
-  button.addEventListener('click', () => {
-    const faqContent = button.nextElementSibling;
-    const isOpen = faqContent.style.display === 'block';
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+import pathSprite from '../../img/sprite.svg';
 
-    faqContent.style.display = isOpen ? 'none' : 'block';
 
-    button.classList.toggle('rotated');
-  });
+const accordion = new Accordion('.faq-list', {
+  duration: 250,
+  showMultiple: true,
+  triggerClass: "faq-item-button",
 });
+
+
+
+// document.querySelectorAll('.toggle-btn').forEach(button => {
+//   button.addEventListener('click', () => {
+//     const faqContent = button.nextElementSibling;
+//     const isOpen = faqContent.style.display === 'block';
+
+//     faqContent.style.display = isOpen ? 'none' : 'block';
+
+//     button.classList.toggle('rotated');
+//   });
+// });
