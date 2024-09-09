a4095d8930acd2137682fd864d762f15535adb61
Johnkonstantin88 <141258972+Johnkonstantin88@users.noreply.github.com>
2024-09-09 16:31:08 +0300

    Update faq.js

--- src/js/faq/faq.js
+++ src/js/faq/faq.js
@@ -1,7 +1,5 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-import pathSprite from '../../img/sprite.svg';
-
 
 const accordion = new Accordion('.faq-list', {
   duration: 250,
@@ -11,13 +9,4 @@ const accordion = new Accordion('.faq-list', {
 
 
 
-// document.querySelectorAll('.toggle-btn').forEach(button => {
-//   button.addEventListener('click', () => {
-//     const faqContent = button.nextElementSibling;
-//     const isOpen = faqContent.style.display === 'block';
 
-//     faqContent.style.display = isOpen ? 'none' : 'block';
-
-//     button.classList.toggle('rotated');
-//   });
-// });
