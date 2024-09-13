baf33378960ba241173caaeaece20c8058b4deb4
Katya <katyakhomenko2105@gmail.com>
2024-09-13 12:59:00 +0300

    one commit

--- src/js/faq/faq.js
+++ src/js/faq/faq.js
@@ -1,13 +1,1 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
 
-document.querySelectorAll('.toggle-btn').forEach(button => {
-  button.addEventListener('click', () => {
-    const faqContent = button.nextElementSibling;
-    const isOpen = faqContent.style.display === 'block';
-
-    faqContent.style.display = isOpen ? 'none' : 'block';
-
-    button.classList.toggle('rotated');
-  });
-});
